735d9f9452fe538167b86ced76d245e8ba2a2b48
TaeJin <>
2020-07-21 11:40:32 +0900

    상세 페이지 UI 추가

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Detail.css";
 
 class Detail extends React.Component {
   componentDidMount() {
@@ -10,10 +11,23 @@ class Detail extends React.Component {
   }
 
   render() {
-    const { location } = this.props;
+    const { state } = this.props.location;
 
-    if (location.state) {
-      return <span>{location.state.title}</span>;
+    console.log(state);
+
+    if (state) {
+      return (
+        <div className="detail__container">
+          <img className="detail__image" src={state.poster} alt={state.title} />
+          <div className="detail__data">
+            <div className="detail__title">{state.title}</div>
+            <div className="detail__genres">[{state.genres}]</div>
+            <div className="detail__year">{state.year}</div>
+            <div className="detail__summary">{state.summary}</div>
+            <div>(평점:{state.rating})</div>
+          </div>
+        </div>
+      );
     } else {
       return null;
     }
